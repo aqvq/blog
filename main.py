1bfe2e090b219da4ea234b523006f944dfd0bed3
QAQ <2505940811@qq.com>
2024-11-28 19:47:05 +0800

    更新 main.py，调整博客头部信息格式，添加个人介绍和学习内容

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ from github import Github
 from lxml.etree import CDATA
 from marko.ext.gfm import gfm as marko
 
-MD_HEAD = """## [Juzaizai's BLOG](https://github.com/aqvq/aqvq)<image align="right" src="https://github-readme-stats.vercel.app/api?username=aqvq&show_icons=true&hide_title=true&theme=gradient" />
+MD_HEAD = """## [Juzaizai's BLOG](https://github.com/aqvq/aqvq)
+
 My personal blog using issues and GitHub Actions [RSS Feed](https://raw.githubusercontent.com/{repo_name}/main/feed.xml) 
 
-### Hello World! Hello You! 😄
+### Hello World! Hello You! 😄 <image align="right" src="https://github-readme-stats.vercel.app/api?username=aqvq&show_icons=true&hide_title=true&theme=gradient" />
+
 - 🔭 I’m Juzaizai
 - 📫 Email: 2505940811@qq.com
 - 🌱 I’m currently learning Embodied AI
