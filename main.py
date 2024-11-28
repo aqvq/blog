d2c827f34dc5085d209c71dd95cbdbe1a51e5180
QAQ <2505940811@qq.com>
2024-11-28 19:31:02 +0800

    update main.py

--- main.py
+++ main.py
@@ -288,7 +288,6 @@ def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
     user = login(token)
     me = get_me(user)
     repo = get_repo(user, repo_name)
-    repo.get_issue(0).get_labels().
     # add to readme one by one, change order here
     add_md_header("README.md", repo_name)
     for func in [add_md_firends, add_md_top, add_md_recent, add_md_label, add_md_todo]:
