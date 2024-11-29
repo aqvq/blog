f38871f77c4240b43617d10a3ebfdfa2d8668b6a
QAQ <2505940811@qq.com>
2024-11-29 16:00:45 +0800

    优化新创建问题的筛选逻辑，排除没有内容的问题

--- main.py
+++ main.py
@@ -269,7 +269,7 @@ def format_issue_with_labels(issue: Issue):
 def bundle_new_created_section(repo: Repository, nums: int = 5):
     new_created_issues = []
     for issue in repo.get_issues(state="open"):
-        if LABEL_TOP in issue.get_labels() or LABEL_COVER in issue.get_labels():
+        if LABEL_TOP in issue.get_labels() or LABEL_COVER in issue.get_labels() or not issue.body:
             continue
         new_created_issues.append(issue)
         if len(new_created_issues) >= nums:
